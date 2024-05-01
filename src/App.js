fdf439b1597450c1205ea8f7257802471aec6642
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-05-01 14:20:07 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1>Javascript程式設計，使用react</h1>
       <h2>第一個元件</h2>
-      <MySlider />
-      <MySlider />
-      <MySlider />
+      <span>R: </span><MySlider />
+      <span>G: </span><MySlider />
+      <span>B: </span><MySlider />
       {/* <header className="App-header"> */}
       {/* <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello React! 2024.5.1</h1>
