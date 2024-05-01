b120ae454944d22fecd68a32c758983f2ced5446
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-05-01 11:44:53 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello React! 2024.5.1</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
