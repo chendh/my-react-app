a1c79524c7277fe1b6b313c483115f4436b518ef
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-05-02 16:33:58 +0800

    新增MySlider

---
+++ src/MySlider.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function MySlider(props) {
+  const [value, setValue] = useState(128);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <span>{props.color}</span>
+      <input
+        type="range"
+        width="400"
+        min="0"
+        max="255"
+        value={value}
+        onChange={handleChange}
+      />
+      <span>{value}</span>
+    </div>
+  );
+}
+export default MySlider;
