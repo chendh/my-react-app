ad385835f5e23085386c01b10006fd10aadaf492
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-05-23 16:21:13 +0800

    chore: Update Tic-Tac-Toe component to use new Board component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
 import MySlider from "./components/rgb-panel/MySlider";
 import MyCalculator from "./components/calculator/MyCalculator";
-import Board from "./components/tic-tac-toe/Board";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <h1>MyCalculator</h1>
       <MyCalculator />
       <h1>Tic-Tac-Toe Game</h1>
-      <Board />
+      <TicTacToe />
     </div>
   );
 }
--- src/components/tic-tac-toe/Board.js
+++ src/components/tic-tac-toe/Board.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Square from "./Square";
-import './tictactoe.css';
+import "./tictactoe.css";
 
-function Board({squares}) {
-    const handleClick = (i) =>{
-
-    }
+function Board({ squares }) {
+  const handleClick = (i) => {};
+  let status = "目前狀態";
   return (
-  <>
-  <div className="board-row">
-    <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-    <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
-    <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
-  </div>
-  </>);
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
 
 export default Board;
