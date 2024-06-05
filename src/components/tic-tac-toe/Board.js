0c9b6988a5d8a24c2cea8bd56c9809681a78e085
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-06-05 11:46:19 +0800

    refactor: Optimize Tic-Tac-Toe board click handling

--- src/components/tic-tac-toe/Board.js
+++ src/components/tic-tac-toe/Board.js
@@ -4,6 +4,9 @@ import "./tictactoe.css";
 
 function Board({ xIsNext, squares, onPlay }) {
   const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -11,6 +14,25 @@ function Board({ xIsNext, squares, onPlay }) {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
+  };
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length - 1; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        return squares[a];
+    }
+    return null;
   };
 
   let status = "下一個玩家: " + (xIsNext ? "X" : "O");
