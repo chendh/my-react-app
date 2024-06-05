6fd4074c7b673b73d9c847903a242cfaa8604026
陳定宏(Ding-Horng Chen) <chendh@stust.edu.tw>
2024-06-05 14:27:40 +0800

    refactor: Fix bug in Tic-Tac-Toe board loop condition

--- src/components/tic-tac-toe/Board.js
+++ src/components/tic-tac-toe/Board.js
@@ -27,7 +27,7 @@ function Board({ xIsNext, squares, onPlay }) {
       [0, 4, 8],
       [2, 4, 6],
     ];
-    for (let i = 0; i < lines.length - 1; i++) {
+    for (let i = 0; i <= lines.length - 1; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
         return squares[a];
